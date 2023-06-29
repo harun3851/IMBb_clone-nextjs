87fdf19d949b5555a5b3b525b01847a324fbb7f3
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-06-29 23:02:19 +0300

    create about page

---
+++ imdb_clone-nextjs/src/app/about/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-x-4 px-4">
+      <h1 className="text-2x font-medium text-amber-600">About</h1>
+      <p>
+Welcome to our captivating film website! Here, 
+we celebrate the magic of cinema and provide a platform for all movie enthusiasts.
+ Immerse yourself in a world of cinematic wonders as we explore the art of storytelling, 
+ the power of visual aesthetics, and the impact of unforgettable performances. From classic 
+ masterpieces to cutting-edge indie gems, we offer a diverse selection of film reviews, 
+ thought-provoking articles, and exclusive interviews with industry insiders. Join our 
+ passionate community, share your thoughts, and discover new perspectives on the silver
+  screen. Let us guide you on an unforgettable journey through the realm of cinema, where dreams come alive
+</p>
+    </div>
+  )
+}
--- imdb_clone-nextjs/src/components/Header.jsx
+++ imdb_clone-nextjs/src/components/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
     <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6 ">
       <div className="flex">
     <MenuItem title="HOME" address="/" Icon={AiFillHome}/>
-    <MenuItem title="ABOUT" address="/" Icon={AiFillInfoCircle}/>
+    <MenuItem title="ABOUT" address="/about" Icon={AiFillInfoCircle}/>
     
 </div>
 <div className="flex items-center space-x-5">
