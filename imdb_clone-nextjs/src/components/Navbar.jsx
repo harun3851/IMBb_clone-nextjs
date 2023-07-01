92856eb256d31e08b2f20a6f24f31ea55ca6d438
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-01 23:31:42 +0300

    navbar completed

--- imdb_clone-nextjs/src/components/Navbar.jsx
+++ imdb_clone-nextjs/src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import NavbarItem from './NavbarItem';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-center">
-        <NavbarItem title="Trending" param="fetchTrending"/>
+    <div className="flex justify-center dark:bg-gray-600  bg-amber-100 lg:text-gl p-4 ">
+        <NavbarItem title="Trending" param="fetchTrending" />
         <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
   )
--- imdb_clone-nextjs/src/components/NavbarItem.jsx
+++ imdb_clone-nextjs/src/components/NavbarItem.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+"use client";
+import Link from 'next/link';
+import { useSearchParams } from "next/navigation";
 
-export default function NavbarItem({title, param}) {
+
+export default function NavbarItem({ title , param }) {
+  const searchParams =useSearchParams();
+  const genre = searchParams.get("genre");
   return (
     <div>
          
-         {title}
+      <Link
+      className={`m-4 hover:text-amber-600 font-semibold p-2
+      ${
+        genre && genre === param &&
+        "underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg"
+      }`}
+      href={`/?genre=${param}`}>
 
+      {title}
+
+      </Link>
+ 
     </div>
-  )
+  );
 }
