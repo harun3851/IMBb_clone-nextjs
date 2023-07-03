7e1ba46cc85e4d5bbc6737e1d7449585b2891fd8
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-03 23:53:16 +0300

    result component create to show in browser

--- imdb_clone-nextjs/src/app/page.js
+++ imdb_clone-nextjs/src/app/page.js
@@ -1,6 +1,6 @@
 export const dynamic = "force-dynamic"; // this is the fix
 
-//import Results from "@/components/Results";
+import Results from "@/components/Results";
 
 const API_KEY = process.env.API_KEY;
 
@@ -24,7 +24,7 @@ export default async function Home({ searchParams }) {
 
   return (
     <div>
-
+   <Results results={results} />
     </div>
   );
 }
