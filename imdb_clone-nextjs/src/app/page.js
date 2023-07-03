a8296b9f8beec1612fc5d9dc02a2cef8e1072e2c
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-03 23:38:54 +0300

    .env created and api fetch

--- imdb_clone-nextjs/src/app/page.js
+++ imdb_clone-nextjs/src/app/page.js
@@ -1,14 +1,30 @@
+export const dynamic = "force-dynamic"; // this is the fix
 
+//import Results from "@/components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className=" text-2xl text-center text-red-500">
-      <p>akif ibrahim gece ve gunduz</p> 
-    
-    <p className="text-3xl text-blue-400" >azmin zaferi</p>
-    </div>
+    <div>
 
-    </main>
-  )
+    </div>
+  );
 }
