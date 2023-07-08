1fc49562e4fbbc05fb871c2e2ee9dbc3a1bc75f9
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-08 16:48:02 +0300

    add search ability

--- imdb_clone-nextjs/src/app/layout.js
+++ imdb_clone-nextjs/src/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import Header from '@/components/Header';
 import Providers from './Providers';
 import Navbar from '@/components/Navbar';
+import SearchBox from "@/components/SearchBox";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         <Navbar />       
 
           {/*SearchBox*/}
+          <SearchBox />
         
         {children}
 
